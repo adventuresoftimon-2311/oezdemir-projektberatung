e9701fc736ae08aefeb2357ccd324aef9e7c1da0
Timon Hoffmann <timon@Timons-MacBook-Pro.local>
2026-02-06 23:52:20 +0100

    Revert German images to originals

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import Button from '../components/Button';
-import service1 from '../assets/service1_german.png';
+import service1 from '../assets/service1.png';
 import service2 from '../assets/service2.png';
-import service3 from '../assets/service3_german.png';
+import service3 from '../assets/service3.png';
 import service4 from '../assets/service4.png';
 
 const ServiceItem = ({ title, subtitle, description, detailsText, detailsList, image, isReversed }) => {
